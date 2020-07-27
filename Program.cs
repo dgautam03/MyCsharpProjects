e768e6e861e38f19f5984116e9a5b7b418dbf94d
Dibya.Gatam <dibyagtm50@gmail.com>
2020-07-26 17:51:39 -0700

    Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace PasscodeP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var code = "";
+            while (code != "secret")
+            {
+                Console.WriteLine("What is the passcode?");
+                code = Console.ReadLine();
+
+                if (code != "secret")
+                    Console.WriteLine("You are not authenticated");
+            }
+            Console.WriteLine("You are authenticated");
         }
     }
 }
